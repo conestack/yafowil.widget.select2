11c77f0582662c4ffe8bd084c10a793e64685daf
Robert Niederreiter <office@squarewave.at>
2018-01-31 16:20:50 +0100

    Convert doctests to unittests

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     extras_require = dict(
         test=tests_require,
     ),
-    test_suite="yafowil.widget.select2.tests.test_suite",
+    test_suite="yafowil.widget.select2.tests",
     entry_points="""
     [yafowil.plugin]
     register = yafowil.widget.select2:register
--- src/yafowil/widget/select2/tests.py
+++ src/yafowil/widget/select2/tests.py
@@ -1,30 +1,216 @@
-from interlude import interact
-from pprint import pprint
-from yafowil.tests import pxml
-import doctest
-import unittest
+from node.tests import NodeTestCase
+from node.utils import UNSET
+from yafowil.base import ExtractionError
+from yafowil.base import factory
+from yafowil.tests import fxml
+import yafowil.widget.select2
+import yafowil.loader
 
 
-optionflags = doctest.NORMALIZE_WHITESPACE | \
-              doctest.ELLIPSIS | \
-              doctest.REPORT_ONLY_FIRST_FAILURE
+class TestSelect2Widget(NodeTestCase):
 
-TESTFILES = [
-    'widget.rst',
-]
+    def test_render_single(self):
+        widget = factory(
+            'select2',
+            name='single',
+            props={
+                'required': True
+            })
+        self.assertEqual(widget(), (
+            '<select class="select2" id="input-single" name="single" '
+            'required="required" />'
+        ))
 
+    def test_render_multiple(self):
+        widget = factory(
+            'select2',
+            name='multi',
+            props={
+                'required': True,
+                'multiple': True
+            })
+        self.assertEqual(widget(), (
+            '<input id="exists-multi" name="multi-exists" type="hidden" '
+            'value="exists" /><select class="select2" id="input-multi" '
+            'multiple="multiple" name="multi" required="required" />'
+        ))
 
-def test_suite():
-    return unittest.TestSuite([
-        doctest.DocFileSuite(
-            file,
-            optionflags=optionflags,
-            globs={'interact': interact,
-                   'pprint': pprint,
-                   'pxml': pxml},
-        ) for file in TESTFILES
-    ])
+    def test_render_single_tag_mode(self):
+        widget = factory(
+            'select2',
+            name='single',
+            props={
+                'inputtag': True
+            })
+        self.assertEqual(widget(), (
+            '<input class="select2" id="input-single" name="single" '
+            'value="" />'
+        ))
+
+    def test_render_multiple_tag_mode(self):
+        widget = factory(
+            'select2',
+            name='multi',
+            value=['1', '2'],
+            props={
+                'inputtag': True,
+                'multiple': True
+            })
+        self.assertEqual(widget(), (
+            '<input class="select2" data-multiple=\'true\' id="input-multi" '
+            'name="multi" value="1,2" />'
+        ))
+
+    def test_render_with_vocabulary(self):
+        # Provide a vocabulary if value terms consists of id / label pairs
+        widget = factory(
+            'select2',
+            name='multi',
+            value=['1', '2'],
+            props={
+                'inputtag': True,
+                'multiple': True,
+                'vocabulary': {'1': 'Label 1', '2': 'Label 2'},
+            })
+        self.assertEqual(widget(), (
+            '<input class="select2" data-multiple=\'true\' '
+            'data-vocabulary=\'{"1": "Label 1", "2": "Label 2"}\' '
+            'id="input-multi" name="multi" value="1,2" />'
+        ))
+
+    def test_extract_required(self):
+        widget = factory(
+            'select2',
+            name='single',
+            props={
+                'required': True
+            })
+
+        request = {}
+        data = widget.extract(request)
+        self.assertEqual(data.extracted, UNSET)
+
+        request = {'single': ''}
+        data = widget.extract(request)
+        self.assertEqual(
+            data.errors,
+            [ExtractionError('Mandatory field was empty')]
+        )
+        self.assertEqual(data.extracted, '')
+
+        request = {'single': '1'}
+        data = widget.extract(request)
+        self.assertEqual(data.errors, [])
+        self.assertEqual(data.extracted, '1')
+
+    def test_extract_single_value(self):
+        widget = factory(
+            'select2',
+            name='single',
+            props={
+                'inputtag': True
+            })
+
+        request = {}
+        data = widget.extract(request)
+        self.assertEqual(data.extracted, UNSET)
+
+        request = {'single': ''}
+        data = widget.extract(request)
+        self.assertEqual(data.extracted, '')
+
+        request = {'single': '1'}
+        data = widget.extract(request)
+        self.assertEqual(data.extracted, '1')
+
+    def test_extract_multi_value(self):
+        widget = factory(
+            'select2',
+            name='multi',
+            props={
+                'multiple': True,
+                'inputtag': True
+            })
+
+        request = {}
+        data = widget.extract(request)
+        self.assertEqual(data.extracted, UNSET)
+
+        request = {'multi': ''}
+        data = widget.extract(request)
+        self.assertEqual(data.extracted, [])
+
+        request = {'multi': '1,2'}
+        data = widget.extract(request)
+        self.assertEqual(data.extracted, ['1', '2'])
+
+    def test_extract_with_preset_value(self):
+        widget = factory(
+            'select2',
+            name='multi',
+            value=['1', '2'],
+            props={
+                'multiple': True,
+                'inputtag': True
+            })
+        self.assertEqual(widget(), (
+            '<input class="select2" data-multiple=\'true\' id="input-multi" '
+            'name="multi" value="1,2" />'
+        ))
+
+        request = {'multi': '1,2,3'}
+        data = widget.extract(request)
+        self.assertEqual(widget(data=data), (
+            '<input class="select2" data-multiple=\'true\' id="input-multi" '
+            'name="multi" value="1,2,3" />'
+        ))
+        self.assertEqual(data.extracted, ['1', '2', '3'])
+
+        request = {'multi': ''}
+        data = widget.extract(request)
+        self.assertEqual(widget(data=data), (
+            '<input class="select2" data-multiple=\'true\' id="input-multi" '
+            'name="multi" value="" />'
+        ))
+        self.assertEqual(data.extracted, [])
+
+    def test_display_renderer_empty(self):
+        widget = factory(
+            'select2',
+            name='empty',
+            mode='display')
+        self.assertEqual(widget(),
+            '<div class="display-select2" id="display-empty"></div>'
+        )
+
+    def test_display_renderer_single(self):
+        widget = factory(
+            'select2',
+            name='single',
+            value='foo',
+            props={
+                'vocabulary': [('foo', 'Foo'), ('bar', 'Bar')]
+            },
+            mode='display')
+        self.assertEqual(widget(),
+            '<div class="display-select2" id="display-single">Foo</div>'
+        )
+
+    def test_display_renderer_multiple(self):
+        widget = factory(
+            'select2',
+            name='multi',
+            value=['foo', 'bar'],
+            props={
+                'vocabulary': [('foo', 'Foo'), ('bar', 'Bar')],
+                'multiple': True,
+            },
+            mode='display')
+        self.assertEqual(widget(), (
+            '<ul class="display-select2" '
+            'id="display-multi"><li>Foo</li><li>Bar</li></ul>'
+        ))
 
 
 if __name__ == '__main__':
-    unittest.main(defaultTest='test_suite')                 #pragma NO COVER
+    unittest.main()                                          # pragma: no cover
