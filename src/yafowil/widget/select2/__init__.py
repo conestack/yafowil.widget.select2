5cb512fc2420f1dbf163dd12b034ded1763d574e
Robert Niederreiter <rnix@squarewave.at>
2025-02-11 08:39:24 +0100

    Cleanup resources

--- src/yafowil/widget/select2/__init__.py
+++ src/yafowil/widget/select2/__init__.py
@@ -84,9 +84,10 @@ bootstrap5_resources = wr.ResourceGroup(
 bootstrap5_resources.add(wr.ScriptResource(
     name='select2-js',
     depends='jquery-js',
-    directory=os.path.join(resources_dir, 'select2'),
-    path='yafowil-select2/select2-develop',
-    resource='dist/js/select2.js'
+    directory=os.path.join(resources_dir, 'select2', 'js'),
+    path='yafowil-select2/select2',
+    resource='select2.js',
+    compressed='select2.min.js'
 ))
 bootstrap5_resources.add(wr.ScriptResource(
     name='yafowil-select2-js',
@@ -94,24 +95,28 @@ bootstrap5_resources.add(wr.ScriptResource(
     path='yafowil-select2/bootstrap5',
     depends='select2-js',
     resource='widget.js'
+    # compressed='widget.min.js'
 ))
 bootstrap5_resources.add(wr.StyleResource(
     name='select2-css',
-    directory=os.path.join(resources_dir, 'select2'),
-    path='yafowil-select2/select2-develop',
-    resource='dist/css/select2.css'
+    directory=os.path.join(resources_dir, 'select2', 'css'),
+    path='yafowil-select2/select2',
+    resource='select2.css',
+    compressed='select2.min.css'
 ))
 bootstrap5_resources.add(wr.StyleResource(
-    name='select2-bootstrap5-css',
-    directory=os.path.join(resources_dir, 'select2'),
-    path='yafowil-select2/select2-bootstrap5-theme',
-    resource='select2-bootstrap-5-theme.css'
+    name='select2-bs5-css',
+    directory=os.path.join(resources_dir, 'select2-bs5'),
+    path='yafowil-select2/select2-bs5',
+    depends='select2-css',
+    resource='select2-bootstrap-5-theme.css',
+    compressed='select2-bootstrap-5-theme.min.css'
 ))
 bootstrap5_resources.add(wr.StyleResource(
     name='yafowil-select2-css',
     directory=os.path.join(resources_dir, 'bootstrap5'),
     path='yafowil-select2/bootstrap5',
-    depends='select2-css',
+    depends='select2-bs5-css',
     resource='widget.min.css'
 ))
 
